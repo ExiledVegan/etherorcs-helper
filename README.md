f827e2236a130ef0fd51a2ff6b828e1bed6ad3b8
ExiledVegan <exiledvegan@gmail.com>
2022-07-26 10:36:37 +0300

    Updated fork with original project changes

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
-# etherorcs-helper
-Chrome Extension for EtherOrcs. Adds some QOL improvements to https://etherorcs.com/ sites.
-
-# Instructions
-
-1. Download and unpack this repository
-2. Enter `chrome://extensions/` in your address bar
-3. Activate the Developer Mode (top right corner)
-4. Press `Load unpacked` and select the unpacked directory
-
-## Loadout
-
-1. Visit `https://dungeons.etherorcs.com/?start` and press `SELECT CHARACTERS`, note that every item has a number now
-2. Open `settings.js` of this extension
-3. Replace the `0` values with the according id/numbers, `id` = you characters #id `ar` = armor, `mh` = mainhand, `oh` = offhand
-4. Save your changes and reload the extension in `chrome://extensions/`
-5. Visit `https://dungeons.etherorcs.com/?start` and press `Use Loadout`
-6. Check if every character was equipped correctly
+<h2>EtherOrcs Companion - Firefox Extension</h2>
+<h3>Chrome Extension: https://github.com/blumentopf-eth/etherorcs-helper </h3>
+<hr>
+<p>The purpose of this project is to further improve the EtherOrcs experience with various tweaks and fixes.</p>
+<p>In the current stage all the improvements are aimed towards the dungeons subdomain. Future versions will aim to improve
+other parts of the website as well, such as the town and game terminal.</p>
+<p>Features:</p>
+<ul>
+    <li>Instant loading of all the in-game animations</li>
+    <li>Remove dungeon shadow effect</li>
+    <li>Equip your own loadout</li>
+    <li>Added a menu to dungeon page with access to other orc links</li>
+</ul>
+<hr>
+<h3>Installing</h3>
+<p>In Firefox: Open the about:debugging page, click the This Firefox option, click the Load Temporary Add-on button, then select any file in your extension's directory.</p>
+<p>The extension now installs, and remains installed until you restart Firefox.</p>
+<p>Alternatively, you can run the extension from the command line using the web-ext tool.</p>
+<hr>
+<h3> Loadout </h3>
+<p>1. Visit https://dungeons.etherorcs.com/?start and press `SELECT CHARACTERS`, note that every item has a number now</p>
+<p>2. Open `settings.js` of this extension</p>
+<p>3. Replace the `0` values with the according id/numbers, `id` = you characters #id `ar` = armor, `mh` = mainhand, `oh` = offhand</p>
+<p>4. Save your changes and reload the extension in about:debugging#/runtime/this-firefox</p>
+<p>5. Visit https://dungeons.etherorcs.com/?start and press `Use Loadout`</p>
+<p>6. Check if every character was equipped correctly</p>
---
+++ js/loadout.js
@@ -0,0 +1,102 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function loadout() {
+    $(".character-select-wrapper .select-map-button").click();
+    await sleep(1500);
+    $(".character-selector .character").each(function () {
+        const orcNumber = $(this).find(".MuiListItemText-primary").text().replace(/\D/g, '');
+        const orcLevel = parseInt($(this).find(".MuiListItemText-secondary").text().replace(/\D/g, '')) - ($(this).hasClass("disabled") ? 100000 : 0);
+        let race = "orc";
+        if (orcNumber > 5050) race = "shaman";
+        if (orcNumber > 8050) race = "ogre";
+        if (orcNumber > 11050) race = "rogue";
+        if (orcNumber > 14050) race = "mage";
+
+        if (!$(this).hasClass("race_" + race)) $(this).addClass("race_" + race);
+        $(this).attr("data-sort", orcLevel);
+        $(this).attr("data-id", orcNumber);
+    });
+
+    let cfg = cfg_loadout.solo;
+    if ($(".character-select-wrapper button[aria-pressed='true']").attr("value") == "party") {
+        cfg = cfg_loadout.party;
+    }
+
+    for (let i = 0; i < cfg.length; i++) {
+        let cfgitem = cfg[i];
+
+        if (cfgitem.id < 1) continue;
+
+        $(".character-selector-inner .character[data-id='" + cfgitem.id + "'] button").click();
+
+        let itemSlot = $(".character-overview .item-slot");
+
+        itemSlot.eq(0).click();
+
+        await sleep(1500);
+
+        if (cfgitem.ar > 1) $(".inventory button").eq(cfgitem.ar - 1).click(); //AR
+
+        itemSlot.eq(1).click();
+
+        await sleep(500);
+
+        if (cfgitem.mh > 1) $(".inventory button").eq(cfgitem.mh - 1).click(); //MH
+
+        itemSlot.eq(2).click();
+
+        await sleep(500);
+
+        if (cfgitem.oh > 1) {
+            $(".inventory button").eq(cfgitem.oh - 1).click(); //OH
+            await sleep(500);
+        }
+    }
+
+    $(".character-selector-inner").prepend($(".character-selector-inner .character.selected"));
+}
+
+$(document).ready(function () {
+    $(document).on("click", ".charSelFilter a", function () {
+        const filterRace = $(this).attr("id").replace("filter_", "");
+        if (filterRace == "all") {
+            $(".character-selector .character").show();
+        } else {
+            $(".character-selector .character").hide();
+            $(".character-selector .character.race_" + filterRace).show();
+        }
+        $(".charSelFilter a").removeClass("specialActive");
+        $(this).addClass("specialActive");
+    });
+    const body = $("body");
+
+    body.append('<div id="specialIcon" class="toggleSpecialMenu">Menu</div>');
+    body.append(`<div id="specialMenu"><ul>
+	<li><a href="https://etherorcs.com/dapp" target="_blank">Terminal</a></li>
+	<li><a href="https://dungeons.etherorcs.com/?start" target="_blank">Dungeons</a></li>
+	<li><a href="https://etherorcs.com/town/emporium" target="_blank">Open Chests</a></li>
+	<li><a href="https://etherorcs.com/town/crafting" target="_blank">Blacksmith</a></li>
+	<li><a href="https://etherorcs.com/town/marketplace" target="_blank">Marketplace</a></li>
+	<li><a class="toggleSpecialMenu" href="#">[close]</a></li>
+	</ul></div>`);
+    $(".toggleSpecialMenu").click(function (e) {
+        e.preventDefault();
+        $("#specialMenu").toggle();
+    });
+
+
+    if ($(".dungeon-crawler").length > 0) {
+        $(document).on('DOMSubtreeModified', function () {
+            let mapBtn = $(".character-select-wrapper .select-map-button");
+            if (mapBtn.length > 0 && $("#loadout").length < 1) {
+                mapBtn.after('<div id="loadout">Use Loadout</div>');
+                $("#loadout").click(function () {
+                    $(this).hide();
+                    loadout();
+                });
+            }
+        });
+    }
+});
